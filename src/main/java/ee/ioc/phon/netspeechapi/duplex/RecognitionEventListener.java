9da30d48ec08bc4a121a9100e2d219bcd424751d
Tjeerd Hans <tjeerdhans@gmail.com>
2018-12-13 16:17:09 +0100

    Added onError and onOpen

--- src/main/java/ee/ioc/phon/netspeechapi/duplex/RecognitionEventListener.java
+++ src/main/java/ee/ioc/phon/netspeechapi/duplex/RecognitionEventListener.java
@@ -3,6 +3,7 @@ package ee.ioc.phon.netspeechapi.duplex;
 public interface RecognitionEventListener {
 
 	void onRecognitionEvent(RecognitionEvent event);
-	
+	void onError();
+	void onOpen();
 	void onClose();
 }
