e9fafa2f703709959ad3245af6841c7b5d59f92e
Tjeerd Hans <tjeerdhans@gmail.com>
2018-12-13 16:30:28 +0100

    added parameters

--- src/main/java/ee/ioc/phon/netspeechapi/duplex/RecognitionEventListener.java
+++ src/main/java/ee/ioc/phon/netspeechapi/duplex/RecognitionEventListener.java
@@ -3,7 +3,7 @@ package ee.ioc.phon.netspeechapi.duplex;
 public interface RecognitionEventListener {
 
 	void onRecognitionEvent(RecognitionEvent event);
-	void onError();
-	void onOpen();
-	void onClose();
+	void onError(Exception exception);
+	void onOpen(ServerHandshake serverHandshake);
+	void onClose(int code, String reason, boolean remote);
 }
